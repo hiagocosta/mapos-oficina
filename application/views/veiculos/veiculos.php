38573796df30ce1fcc0c887370c30ec35bfbc084
hiagocosta <22087481+hiagocosta@users.noreply.github.com>
2026-03-21 22:53:21 -0300

    Create veiculos.php to list vehicles with actions

---
+++ application/views/veiculos/veiculos.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Veículos</title>
+</head>
+<body>
+    <h1>Lista de Veículos</h1>
+    <input type="text" id="search" placeholder="Pesquisar veículos..."></input>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody id="vehicleList">
+            <!-- Vehicle rows will be populated here -->
+        </tbody>
+    </table>
+
+    <script>
+        // JavaScript to handle search, edit, view and delete will go here
+    </script>
+</body>
+</html>
